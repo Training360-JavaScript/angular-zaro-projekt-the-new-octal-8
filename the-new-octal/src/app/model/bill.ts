0dac0e0d4b26181fd831b4069144e6d2ee801592
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2022-02-08 18:40:45 +0100

    Models added.

---
+++ the-new-octal/src/app/model/bill.ts
@@ -0,0 +1,17 @@
+export class Bill {
+  [key: string]: any;
+  id: number = 0;
+  orderID: number = 0;
+  amount: number = 0;
+  status: string = ''; //"new|paid"
+
+  constructor(properties?: Bill) {
+    if (properties) {
+      this.id = properties.id || 0;
+      this.orderID = properties.orderID || 0;
+      this.amount = properties.amount || 0;
+      this.status = properties.status || '';
+    }
+  }
+
+}
---
+++ the-new-octal/src/app/model/customer.ts
@@ -0,0 +1,24 @@
+import { Address } from "./address";
+
+export class Customer {
+  [key: string]: any;
+  id: number = 0;
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  address: Address = new Address();
+  //address?: Address;
+  active: boolean = false;
+
+  constructor(properties?: Customer) {
+    if (properties) {
+      this.id = properties.id || 0;
+      this.firstName = properties.firstName || '';
+      this.lastName = properties.lastName || '';
+      this.email = properties.email || '';
+      this.address = properties.address || undefined;
+      this.active = properties.active || false;
+    }
+  }
+
+}

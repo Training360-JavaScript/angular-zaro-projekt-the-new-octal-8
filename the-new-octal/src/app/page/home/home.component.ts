63a8b9fe3f64bd5274bb01f882d940c020eeeb76
BKMarcsi <bkolonuszmaria@gmail.com>
2022-02-12 11:05:47 +0100

    mock data a felületre

--- the-new-octal/src/app/page/home/home.component.ts
+++ the-new-octal/src/app/page/home/home.component.ts
@@ -9,6 +9,15 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  activeProducts: number = 27; //mock data
+
+  activeCustomers: number = 155; //mock data
+
+  notPaidOrders: number = 15; //mock data
+
+  sumNotPaidBills: number = 15750; //mock data
+
+
   ngOnInit(): void {
   }
 

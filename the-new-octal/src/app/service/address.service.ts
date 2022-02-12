0e76571b37278690fdeac0ad88467fbecd3d7f9e
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2022-02-13 00:19:03 +0100

    Address and its service changed with ID

--- the-new-octal/src/app/service/address.service.ts
+++ the-new-octal/src/app/service/address.service.ts
@@ -3,46 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Address } from '../model/address';
+import { BaseService } from './base.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 
-export class AddressService {
-
-  apiUrl: string = environment.apiUrl;
-  entityName: string = 'address';
+export class AddressService extends BaseService<Address>{
 
   constructor(
-    private http: HttpClient
-  ) { }
-
-  getAll(): Observable<Address[]> {
-    return this.http.get<Address[]>(`${this.apiUrl}${this.entityName}`);
-  }
-
-  create(entity: Address): Observable<Address> {
-    return this.http.post<Address>(`${this.apiUrl}${this.entityName}`, entity);
-  }
-
-/*
-  get(id: number): Observable<T> {
-    return this.http.get<T>(`${this.apiUrl}${this.entityName}/${id}`);
-  }
-
-
-  update(entity: T): Observable<T> {
-    return this.http.patch<T>(
-      `${this.apiUrl}${this.entityName}/${entity.id}`,
-      entity
-    );
-  }
-
-  delete(id: number): Observable<T> {
-    return this.http.delete<T>(`${this.apiUrl}${this.entityName}/${id}`);
-  }
-
- */
+    public override http: HttpClient
+  ) {
+    super(http);
+    this.entityName = 'address';
+   }
 
 }

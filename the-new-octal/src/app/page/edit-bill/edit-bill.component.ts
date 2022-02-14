481fc6f5e0644695b4ecc88b4311d5248f2066d1
Márk Fazekas <coldrex.fazekas.mark@gmail.com>
2022-02-14 06:17:24 +0100

    (feature): Edit-bill - 17

--- the-new-octal/src/app/page/edit-bill/edit-bill.component.ts
+++ the-new-octal/src/app/page/edit-bill/edit-bill.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Observable, switchMap} from "rxjs";
+import {BillService} from "../../service/bill.service";
+import {Bill} from "../../model/bill";
 
 @Component({
   selector: 'app-edit-bill',
@@ -8,12 +11,40 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditBillComponent implements OnInit {
 
-  bill!: any;
+  editedObject: Bill | undefined = undefined;
+  edit: boolean = true;
+  listOfStatusValues: string[] = ["new", "paid"];
+  mainComponentRoute = "bills";
+
   constructor(
-    private ar: ActivatedRoute
-  ) { }
+    private router: Router,
+    private ar: ActivatedRoute,
+    private objectService: BillService
+  ) {
+    this.ar.params.pipe(
+      switchMap(params => this.objectService.get(params['id']))
+    ).subscribe(
+      currentObject => {
+        if (currentObject === null || currentObject === undefined || currentObject.id < 1) {
+          this.edit = false;
+          this.editedObject = new Bill();
+        } else {
+          this.editedObject = currentObject
+        }
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
 
+  onSend(editedObject: Bill) {
+    const crudObservable: Observable<any> = this.edit ? this.objectService.update(editedObject) : this.objectService.create(editedObject);
+    crudObservable.subscribe(
+      result => {
+        console.log(editedObject, result);
+        this.router.navigate([this.mainComponentRoute])
+      }
+    )
+  }
 }
